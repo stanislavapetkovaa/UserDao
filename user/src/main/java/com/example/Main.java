5ba095503262a708fe5d28ac36f79e9a3242cc4d
stanislavapetkovaa <stanislavapetkovagaming@gmail.com>
2023-11-20 19:42:53 +0200

    new

--- user/src/main/java/com/example/Main.java
+++ user/src/main/java/com/example/Main.java
@@ -2,14 +2,15 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new User(3, "Test5", "2", "3");
+        User user = new User(3, "Test5", "3", "3");
         UserDao userDao = new UserDao(user);
-        System.out.println(userDao.getallUsers().get(0).toString());
-      
-        // userDao.insertToDB(user);
+        userDao.insertToDB(user);
 
-        // userDao.updateFirstName(user,"Update");
-        // userDao.getUserById(2);
+        userDao.updateFirstName(user,"Update");
+        userDao.getUserById(2);
+        System.out.println(userDao.getallUsers().get(0).getFirstName().toString());
+      
+        
         //userDao.deleteUser(user);
 
     
